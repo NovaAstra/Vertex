e57c5a682e6db1d296ad356c73e0bcc46de34005
AugustGao <August.Gao@mspbots.ai>
2024-11-04 11:26:12 +0800

    初始化

--- packages/finitio/demo/index.js
+++
@@ -1,9 +0,0 @@
-// TODO : export only the two/three functions part of the API! might require updating tests imports
-// - move the converter to yed2kingly or a new single script with options (convert to knogly, to uml, etc.)
-// export * from './types'
-export {createStateMachine, createPureStateMachine, makeWebComponentFromFsm, historyState} from './synchronous_fsm'
-export * from './converter'
-export {ACTION_IDENTITY, INIT_EVENT, INIT_STATE, SHALLOW, DEEP, NO_STATE_UPDATE, NO_OUTPUT} from './properties'
-export {computeHistoryMaps, initHistoryDataStructure} from './helpers'
-export {fsmContracts} from './contracts'
-
