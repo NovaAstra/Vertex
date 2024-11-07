767f330df6f9a51f85e43dacf805b3e5767ed9b0
GaoYu <NovaAstra@outlook.com>
2024-11-07 22:55:33 +0800

    main

--- packages/elation/curry.ts
+++ packages/elation/curry.ts
@@ -1,11 +1,22 @@
-export type Curried<T extends unknown[], R> = T extends [] ? R : ''
+// export type Curried<T extends unknown[], R> = T extends [] ? R : ''
 
 export function curry<T extends unknown[], R>(
     fn: (...args: T) => R,
     ...args: Partial<T>
-) {
-    const curried = (..._args: T) =>
-        _args.length >= fn.length ? fn(..._args) : curry(fn, ..._args)
-
-    return curried.bind(null, ...args)
+): T["length"] extends 0 ? () => R : any {
+    const curried = (..._args: T): R => {
+        const combined = args.concat(_args) as T;
+        return combined.length >= fn.length ?
+            fn(...combined) :
+            curried.bind(null, ...combined)
+    }
+    return curried
 }
+
+const fn = (a, b, c) => a + b + c
+const curried = curry(fn, 1, 2, 3)()
+// const sum = curried(1)
+
+// const result = sum(3) // => 6
+
+console.log(curried)
