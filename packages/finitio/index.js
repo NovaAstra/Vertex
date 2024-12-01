31a0e5fcc3f6fdaf57bdc4e9678068ef1e8e284a
GaoYu <NovaAstra@outlook.com>
2024-12-01 21:47:22 +0800

    main

---
+++ packages/finitio/index.js
@@ -0,0 +1,44 @@
+class MemberImpl {
+    constructor(tag, data, tagProp) {
+        this.data = data
+        Object.assign(this, {
+            [tagProp]: tag,
+            getTag: () => this[tagProp]
+        })
+    }
+}
+
+function matchbox(tag, data, tagProp = 'tag') {
+    return new MemberImpl(tag, data, tagProp)
+}
+
+function matchboxFactory(config, tagProp = 'tag') {
+    const createObj = {}
+    for (const tag in config) {
+        const spec = (config)[tag];
+        createObj[tag] = (...args) => {
+            return matchbox(
+                tag,
+                typeof spec === 'function' ? spec(args) : spec,
+                tagProp
+            )
+        }
+    }
+    return createObj;
+}
+
+function defineStates(config) {
+    return matchboxFactory(config, "key");
+}
+
+const config = {
+    Idle: undefined,
+    Loading: { id: 1 },
+    Loaded: (data) => ({ data }),
+    Error: (error) => ({ error }),
+}
+
+const state = defineStates(config)
+
+console.log(state.Loaded(22))
+
