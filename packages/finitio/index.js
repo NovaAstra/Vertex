6233c344280b5c991b3ec812b1970a9e9c65800a
AugustGao <August.Gao@mspbots.ai>
2024-12-02 16:47:16 +0800

    main

--- packages/finitio/index.js
+++ packages/finitio/index.js
@@ -31,23 +31,31 @@ function defineStates(config) {
     return matchboxFactory(config, "key");
 }
 
-const config = {
-    Idle: undefined,
-    Loading: { id: 1 },
-    Loaded: (data) => ({ data }),
-    Error: (error) => ({ error }),
-}
 
-const state = defineStates(config)
-
-function createStateMachine(transitions, initialStates) {
+function createStateMachine(transitions, initialState) {
     let lastChange = {
         type: "__initialize",
         to: initialState,
     }
 
     const machine = {
-        transitions
+        transitions,
+        getChange: () => lastChange,
+        getState: () => lastChange.to,
+        send: (type, ...params) => {
+            const lastChange = machine.getChange();
+            const resolved = machine.resolve({
+                type,
+                params,
+                from: lastChange.to,
+            })
+        },
+        resolve: (ev) => {
+            const to = machine.transitions[ev.from.key][ev.type];
+            if (to) {
+                return { ...ev, to }
+            }
+        }
     }
 
     return machine
@@ -58,6 +66,20 @@ function createFactoryMachine(states, transitions, init) {
         typeof init === "string" ? states[init]({}) : init
     );
     const machine = createStateMachine(transitions, initialState);
-    Object.assign(machine, {})
+    Object.assign(machine, {
+        states,
+        resolve: () => {
+
+        }
+    })
     return machine
 }
+
+const config = {
+    Idle: undefined,
+    Loading: { id: 1 },
+    Loaded: (data) => ({ data }),
+    Error: (error) => ({ error }),
+}
+
+const state = defineStates(config)
