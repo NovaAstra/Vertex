db67bd705282d325cc4b2e1dedd7cb7d2e952b3c
AugustGao <August.Gao@mspbots.ai>
2024-12-06 11:50:58 +0800

    main

--- packages/finitio/machine.ts
+++
@@ -1,7 +0,0 @@
-import { StateMachine, MachineSchema } from "./StateMachine"
-
-export function createMachine<Context, Event, Meta>(
-    schema: MachineSchema<Context, Event, Meta>
-) {
-    return new StateMachine<Context, Event, Meta>(schema)
-}
